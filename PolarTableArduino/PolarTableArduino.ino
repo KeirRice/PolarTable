a093d75e5ef6a9a1d4e6986d7402ed570b4421fa
Keir Rice <keirrice@gmail.com>
2018-10-01 23:59:14 +1300

    Started rebuilding the main file.

--- PolarTableArduino/PolarTableArduino.ino
+++ PolarTableArduino/PolarTableArduino.ino
@@ -1,5 +1,39 @@
+#define DEBUG 1
+#include "Debug.h"
+
+// #define ENABLE_SX1509
+
+#include "HelpersTemplate.h"
+#include <FastLED.h>
+#include "pixeltypes.h"
+#include "WireTypes.h"
+#include "SX1509.h"
+
+typedef unsigned int uint;
+
+const byte SX1509_ADDRESS = 0x3E;  // SX1509 I2C address (00)
+SX1509 io;
+
+long startMillis;
+
+void resetMillis(){
+  startMillis = millis();
+}
+
 void setup()
 {
+  if(DEBUG){
+    Serial.begin(9600);
+  }
+  
+  DEBUG_PRINTLN("setup coms.");
+  DEBUG_WHERE();
+  resetMillis();
+  if (!io.begin(SX1509_ADDRESS))
+  {
+    DEBUG_PRINTLN("Failed to begin SX1509 coms.");
+    while (1);
+  }
   
 }
 
