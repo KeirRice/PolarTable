12c99e2ff8594c08e43589bbdb1630d630dd2c53
Keir Rice <keirrice@gmail.com>
2018-11-13 13:16:47 +1300

    Added split interface to also test RPi.GPIO

--- Python/bb_i2c_peripheral.py
+++ Python/bb_i2c_peripheral.py
@@ -40,18 +40,178 @@ More reference:
 	https://rheingoldheavy.com/i2c-signals/
 	https://robot-electronics.co.uk/i2c-tutorial
 
+GPIO
+	https://pimylifeup.com/raspberry-pi-gpio/
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install rpi.gpio
+sudo apt-get install python-rpi.gpio
+sudo raspi-config
+lsmod | grep i2c_
+lsmod | grep spi_
+
+
+https://www.raspberrypi.org/forums/viewtopic.php?t=8999
+# dialout is a user group
+echo "4" > /sys/class/gpio/export
+sudo nano /etc/rc.local
+chgrp -R dialout /sys/class/gpio
+chmod -R g+rw /sys/class/gpio
+
+
+https://www.raspberrypi.org/forums/viewtopic.php?p=161013&sid=963ba2183a4c7f5363cc2b911cac8838#p161013
+sudo groupadd gpio
+sudo usermod -aG gpio <myusername>
+su <myusername>
+sudo chgrp gpio /sys/class/gpio/export
+sudo chgrp gpio /sys/class/gpio/unexport
+sudo chmod 775 /sys/class/gpio/export
+sudo chmod 775 /sys/class/gpio/unexport
+
+
+https://www.raspberrypi.org/forums/viewtopic.php?t=118879
+Solution is to edit:
+/lib/udev/rules.d/60-python-pifacecommon.rules and
+/lib/udev/rules.d/60-python3-pifacecommon.rules (if they both exist) to be:
+
+KERNEL=="spidev*", GROUP="spi", MODE="0660"
+SUBSYSTEM=="gpio*", PROGRAM="/bin/sh -c 'chown -R root:gpio /sys/class/gpio && chmod -R 770 /sys/class/gpio; chown -R root:gpio /sys/devices/virtual/gpio && chmod -R 770 /sys/devices/virtual/gpio; chown -R root:gpio /sys/devices/platform/soc/*.gpio/gpio && chmod -R 770 /sys/devices/platform/soc/*.gpio/gpio'"
+
 """
 
-
-import pigpio
 import Queue
 import struct
 
 
+class PinBase(object):
+	"""Wrapper to swap modes when we need to read and write."""
+
+	def __init__(self, pin, pi):
+		"""Init."""
+		self.pi = pi
+		self.pin = pin
+		self.mode = None
+
+		self.setup()
+
+	def setup(self):
+		"""Initial setup."""
+		return NotImplemented
+
+	def set_mode(self, mode):
+		"""Update the pin mode if it needs to change.
+		
+		We keep a local cache of the value and assume it wont get changed
+		externally.
+		"""
+		return NotImplemented
+
+	def __eq__(self, other):
+		"""Equality checks."""
+		if isinstance(other, Pin):
+			return self.pin == other.pin
+		return self.pin == other
+
+	def read(self):
+		"""Read from the pin."""
+		return NotImplemented
+
+	def write(self, value):
+		"""Write to the pin."""
+		return NotImplemented
+
+class PiPin(PinBase):
+	"""Wrapper to swap modes when we need to read and write."""
+
+	def __init__(self, pin, pi):
+		"""Init."""
+		super(PiPin, self).__init__(pin, pi)
+
+	def setup(self):
+		"""Initial setup."""
+		self.pi.set_pull_up_down(self.pin, PUD_OFF)
+
+	def set_mode(self, mode):
+		"""Update the pin mode if it needs to change.
+		
+		We keep a local cache of the value and assume it wont get changed
+		externally.
+		"""
+		if self.mode != mode:
+			self.pi.set_mode(mode)
+			self.mode = mode
+
+	def read(self):
+		"""Read from the pin."""
+		self.set_mode(self.pin, pigpio.INPUT)
+		return self.pi.read(self.pin)
+
+	def write(self, value):
+		"""Write to the pin."""
+		self.set_mode(self.pin, pigpio.OUTPUT)
+		return self.pi.write(self.pin, value)
+
+
+class RpiPin(PinBase):
+	"""Wrapper to swap modes when we need to read and write."""
+
+	def __init__(self, pin, pi):
+		"""Init."""
+		super(RpiPin, self).__init__(pin, pi)
+
+	def setup(self):
+		"""Initial setup."""
+		self.pi.setup(self.pin, self.pi.IN, self.pi.PUD_OFF)
+
+	def set_mode(self, mode):
+		"""Update the pin mode if it needs to change.
+		
+		We keep a local cache of the value and assume it wont get changed
+		externally.
+		"""
+		if self.mode != mode:
+			self.pi.setup(self.pin, mode, PUD_OFF)
+			self.mode = mode
+
+	def read(self):
+		"""Read from the pin."""
+		self.set_mode(self.pin, self.pi.IN)
+		return self.pi.input(self.pin)
+
+	def write(self, output):
+		"""Write to the pin."""
+		self.set_mode(self.pin, self.pi.OUT)
+		return self.pi.output(self.pin, output)
+
+ENABLE_PIGPIO = False
+ENABLE_RPI = True
+if ENABLE_PIGPIO:
+
+	import pigpio
+
+	HIGH = 1
+	LOW = 0
+	PUD_OFF = pigpio.PUD_OFF
+	
+	Pin = PiPin
+
+
+if ENABLE_RPI:
+
+	import RPi.GPIO as GPIO
+	GPIO.setmode(GPIO.BCM)
+	HIGH = GPIO.HIGH
+	LOW = GPIO.LOW
+	PUD_OFF = GPIO.PUD_OFF
+
+	Pin = RpiPin
+
+
 # Globals
 
-HIGH = NACK = 1
-LOW = ACK = 0
+NACK = 1
+ACK = 0
 
 
 class Peripheral(object):
@@ -76,51 +236,19 @@ class Peripheral(object):
 			"""Release control of the clock."""
 			self.pin.write(HIGH)
 
-	class Pin(object):
-		"""Wrapper to swap modes when we need to read and write."""
-
-		def __init__(self, pin, pi):
-			"""Init."""
-			self.pi = pi
-			self.pin = pin
-			self.mode = self.pi.get_mode(pin)
-			self.pi.set_pull_up_down(pin, pigpio.PUD_OFF)
-
-		def set_mode(self, mode):
-			"""Update the pin mode if it needs to change.
-			
-			We keep a local cache of the value and assume it wont get changed
-			externally.
-			"""
-			if self.mode != mode:
-				self.pi.set_mode(mode)
-				self.mode = mode
-
-		def __eq__(self, other):
-			"""Equality checks."""
-			if isinstance(other, Peripheral.Pin):
-				return self.pin == other.pin
-			return self.pin == other
-
-		def read(self):
-			"""Read from the pin."""
-			self.set_mode(self.pin, pigpio.INPUT)
-			return self.pi.read(self.pin)
-
-		def write(self):
-			"""Write to the pin."""
-			self.set_mode(self.pin, pigpio.OUTPUT)
-			return self.pi.write(self.pin)
-
 	def __init__(self, SCL, SDA, address):
 		"""Init."""
-		pi = pigpio.pi()             # exit script if no connection
-		if not pi.connected:
-			raise RuntimeError('No Connection')
+		if ENABLE_PIGPIO:
+			pi = pigpio.pi()             # exit script if no connection
+			if not pi.connected:
+				raise RuntimeError('No Connection')
+		
+		if ENABLE_RPI:
+			pi = GPIO
 		
 		# Pins and address
-		self.SCL = Peripheral.Pin(SCL, pi)
-		self.SDA = Peripheral.Pin(SDA, pi)
+		self.SCL = Pin(SCL, pi)
+		self.SDA = Pin(SDA, pi)
 		self.our_address = address
 
 		# Callbacks for the python side
@@ -145,14 +273,32 @@ class Peripheral(object):
 		self.fall_oneshots = list()
 		self.rise_oneshots = list()
 
-		# Callbacks from the hardware side
-		self.cb1 = self.pi.callback(self.SDA, pigpio.EITHER_EDGE, self.hardware_callback)
-		self.cb2 = self.pi.callback(self.SCL, pigpio.EITHER_EDGE, self.hardware_callback)
+		if ENABLE_PIGPIO:
+			# Callbacks from the hardware side
+			self.cb1 = self.pi.callback(self.SDA, pigpio.EITHER_EDGE, self.hardware_callback)
+			self.cb2 = self.pi.callback(self.SCL, pigpio.EITHER_EDGE, self.hardware_callback)
+
+		if ENABLE_RPI:
+			def hardware_callback_sda():
+				self.hardware_callback(self.SDA, self.SDA.read(), 0)
+
+			def hardware_callback_scl():
+				self.hardware_callback(self.SCL, self.SCL.read(), 0)
+
+			GPIO.add_event_detect(self.SDA, GPIO.BOTH)
+			GPIO.add_event_callback(self.SDA, hardware_callback_sda)
+			GPIO.add_event_detect(self.SCL, GPIO.BOTH)
+			GPIO.add_event_callback(self.SCL, hardware_callback_scl)
 
 	def cancel(self):
 		"""Clean up the callbacks."""
-		self.cb1.cancel()
-		self.cb2.cancel()
+		if ENABLE_PIGPIO:
+			self.cb1.cancel()
+			self.cb2.cancel()
+
+		if ENABLE_RPI:
+			GPIO.remove_event_detect(self.SDA)
+			GPIO.remove_event_detect(self.SCL)
 
 	def reset_state(self):
 		"""Clear out any state for a next transaction."""
@@ -536,7 +682,7 @@ class Registers(object):
 
 
 def main():
-	SDA, SCL = 4, 5
+	SDA, SCL = 2, 3
 	ADDRESS = '\x14'
 	wire = Peripheral(SCL, SDA, ADDRESS)
 	try:
