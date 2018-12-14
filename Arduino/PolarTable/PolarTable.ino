e73799f07cd65361bd3271a8ea337af593a188d0
Keir Rice <keirrice@gmail.com>
2018-12-14 17:26:46 +1300

    Sync Pin refactor

--- Arduino/PolarTable/PolarTable.ino
+++ Arduino/PolarTable/PolarTable.ino
@@ -34,8 +34,9 @@ typedef struct FsmEventDriver : public EventTask
 #define EI_NOTEXTERNAL
 #include <EnableInterrupt.h>
 #include <kEncoder.h>
-kEncoder::AbsoluteEncoder abs_encoder(kEncoder::PortGroup(kPin::PORT_K, 0b00001111));
-kEncoder::RelativeEncoder rel_encoder(kEncoder::PortGroup(kPin::PORT_K, 0b00110000));
+kEncoder::AbsoluteEncoder abs_encoder(kPin::Group::Port(kPin::PORT_K, 0b00001111));
+kEncoder::RelativeEncoder rel_encoder(kPin::Group::Port(kPin::PORT_K, 0b00110000));
+
 
 void abs_encoder_isr(){
   abs_encoder.interputHandler();
